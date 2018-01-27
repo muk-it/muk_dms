6aaec7d1932ecf7e305eaa0c04ccbf2024317b1e
Mathias Markl <mathias.markl@mukit.at>
2018-01-27 14:33:18 +0100

    german

--- muk_dms_file/__manifest__.py
+++ muk_dms_file/__manifest__.py
@@ -25,7 +25,7 @@
     "description": """ 
         
     """,
-    "version": '11.0.1.2.0',   
+    "version": '11.0.1.2.1',   
     "category": 'Document Management',   
     "license": "AGPL-3",
     "website": "http://www.mukit.at",
