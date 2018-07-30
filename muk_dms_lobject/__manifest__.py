d6de0e6a44de3a3808c98db5672fd1067c6ea5e3
MuK IT GmbH <mathias.markl@mukit.at>
2018-07-30 22:25:37 +0000

    publish muk_dms_lobject - 11.0.2.0.1

--- muk_dms_lobject/__manifest__.py
+++ muk_dms_lobject/__manifest__.py
@@ -22,7 +22,7 @@
 {
     "name": "MuK Documents Large Object",
     "summary": """Document Large Object Support""",
-    "version": '11.0.2.0.0',   
+    "version": '11.0.2.0.1',   
     "category": 'Document Management',   
     "license": "AGPL-3",
     "website": "http://www.mukit.at",
