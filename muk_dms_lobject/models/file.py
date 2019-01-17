ed7cf4b9f24d8c0425b2c698408e2c961e8fc6bf
MuK IT GmbH <mathias.markl@mukit.at>
2019-01-17 19:50:40 +0000

    publish muk_dms_lobject - 12.0

---
+++ muk_dms_lobject/models/file.py
@@ -0,0 +1,99 @@
+###################################################################################
+# 
+#    Copyright (C) 2018 MuK IT GmbH
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###################################################################################
+
+import base64
+import logging
+
+from collections import defaultdict
+
+from odoo import models, api, fields, tools
+
+from odoo.addons.muk_fields_lobject.fields import lobject
+
+_logger = logging.getLogger(__name__)
+
+class File(models.Model):
+    
+    _inherit = 'muk_dms.file'      
+             
+    #----------------------------------------------------------
+    # Database
+    #----------------------------------------------------------
+    
+    content_lobject = lobject.LargeObject(
+        string="Content LObject",
+        prefetch=False,
+        invisible=True)
+    
+    #----------------------------------------------------------
+    # Helper
+    #----------------------------------------------------------
+
+    @api.model
+    def _get_content_vals(self):
+        res = super(File, self)._get_content_vals()
+        res.update({'content_lobject': False})
+        return res
+        
+        
+    
+    #----------------------------------------------------------
+    # Read, View 
+    #---------------------------------------------------------- 
+    
+    @api.depends('content_lobject')     
+    def _compute_content(self):
+        bin_size = self.env.context.get('bin_size')
+        bin_recs = self.with_context({'bin_size': True})
+        records = bin_recs.filtered(lambda rec: bool(rec.content_lobject))
+        for record in records.with_context(self.env.context):
+            context = {'human_size': True} if bin_size else {'base64': True}
+            record.content = record.with_context(context).content_lobject
+        super(File, self - records)._compute_content()
+    
+    @api.depends('content_lobject')
+    def _compute_save_type(self):
+        bin_size = self.env.context.get('bin_size')
+        bin_recs = self.with_context({'bin_size': True})
+        records = bin_recs.filtered(lambda rec: bool(rec.content_lobject))
+        for record in records.with_context(self.env.context):
+            record.save_type = "lobject"
+        super(File, self - records)._compute_save_type()
+    
+    #----------------------------------------------------------
+    # Create, Update, Delete
+    #----------------------------------------------------------
+    
+    @api.multi
+    def _inverse_content(self):
+        records = self.filtered(
+            lambda rec: rec.storage.save_type == 'lobject'
+        )
+        updates = defaultdict(set)
+        for record in records:
+            vals = {
+                'content_lobject': record.content,
+                'size': len(base64.b64decode(record.content or "")),
+            }
+            init = self._get_content_vals()
+            updates[tools.frozendict({**init, **vals})].add(record.id)
+        with self.env.norecompute():
+            for vals, ids in updates.items():
+                self.browse(ids).write(dict(vals))
+        super(File, self - records)._inverse_content()
---
+++ muk_dms_lobject/tests/test_file.py
@@ -0,0 +1,36 @@
+###################################################################################
+# 
+#    Copyright (C) 2017 MuK IT GmbH
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###################################################################################
+
+import os
+import logging
+
+from odoo.exceptions import AccessError, ValidationError
+
+from odoo.addons.muk_utils.tests.common import multi_users
+from odoo.addons.muk_dms.tests.common import setup_data_function
+from odoo.addons.muk_dms.tests.test_file import FileTestCase
+
+_path = os.path.dirname(os.path.dirname(__file__))
+_logger = logging.getLogger(__name__)
+
+class FileLObjectTestCase(FileTestCase):
+
+    def _setup_test_data(self):
+        super(FileLObjectTestCase, self)._setup_test_data()
+        self.new_storage.write({'save_type': 'lobject'})
