c712325a7baef756fd863d78b72e9b9d036c803b
MuK IT GmbH <mathias.markl@mukit.at>
2019-04-16 10:11:23 +0000

    publish muk_dms - 12.0

--- muk_dms/__manifest__.py
+++ muk_dms/__manifest__.py
@@ -20,7 +20,7 @@
 {
     "name": "MuK Documents", 
     "summary": """Document Management System""",
-    "version": '12.0.2.7.18',   
+    "version": '12.0.2.7.19',   
     "category": 'Document Management',   
     "license": "AGPL-3",
     "website": "http://www.mukit.at",
