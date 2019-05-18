67382345c1c6d6229ece642f801d5a933f22c40d
MuK IT GmbH <mathias.markl@mukit.at>
2019-05-18 16:09:07 +0000

    publish muk_dms - 12.0

--- muk_dms/controllers/__init__.py
+++ muk_dms/controllers/__init__.py
@@ -17,23 +17,5 @@
 #
 ###################################################################################
 
+from . import backend
 from . import onboarding
-
-from odoo import http
-from odoo.http import request
-
-class OnboardingController(http.Controller):
-
-    @http.route('/dms/document_onboarding', auth='user', type='json')
-    def document_onboarding(self):
-        company = request.env.user.company_id
-        if not request.env.user._is_admin() or \
-           company.sale_quotation_onboarding_state == 'closed':
-            return {}
-
-        return {
-            'html': request.env.ref('sale.sale_quotation_onboarding_panel').render({
-                'company': company,
-                'state': company.get_and_update_sale_quotation_onboarding_state()
-            })
-        }
---
+++ muk_dms/controllers/backend.py
@@ -0,0 +1,34 @@
+###################################################################################
+# 
+#    Copyright (C) 2017 MuK IT GmbH
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###################################################################################
+
+import logging
+
+from odoo import _, http
+from odoo.http import request
+
+_logger = logging.getLogger(__name__)
+
+class BackendController(http.Controller):
+
+    @http.route('/config/muk_dms.forbidden_extensions', type='json', auth="user")
+    def forbidden_extensions(self, **kw):
+        params = request.env['ir.config_parameter'].sudo()
+        return {
+            'forbidden_extensions': params.get_param("muk_dms.forbidden_extensions", default="")
+        }
