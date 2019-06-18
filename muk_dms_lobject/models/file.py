b43eff3a506b59d398cc713063f2955eec400da5
MuK IT GmbH <mathias.markl@mukit.at>
2019-06-18 10:02:48 +0000

    publish muk_dms_lobject - 12.0

--- muk_dms_lobject/models/file.py
+++ muk_dms_lobject/models/file.py
@@ -88,7 +88,7 @@ class File(models.Model):
             binary = base64.b64decode(record.content or "")
             values = self._update_content_vals(record, values, binary)
             values.update({
-                'content_lobject': record.content,
+                'content_lobject': record.content and binary,
             })
             updates[tools.frozendict(values)].add(record.id)
         with self.env.norecompute():
