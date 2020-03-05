f7844d6884e3498ba856859eee119c2ad0dd1c2f
Mathias Markl <mathias.markl@mukit.at>
2020-03-05 11:35:45 +0100

    Update __manifest__.py

--- muk_dms_share/__manifest__.py
+++ muk_dms_share/__manifest__.py
@@ -22,7 +22,7 @@
     "summary": """Share Documents""",
     "version": '11.0.2.0.1',   
     "category": 'Document Management',   
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "website": "http://www.mukit.at",
     "live_test_url": "https://demo.mukit.at/web/login",
     "author": "MuK IT",
